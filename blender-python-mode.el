b09da7482aa4d0229ffd4ac573c69587e12e9d70
Jonas Bernoulli <jonas@bernoul.li>
2013-01-27 02:57:41 +0100

    add summary line, fix header

--- blender-python-mode.el
+++ blender-python-mode.el
@@ -1,34 +1,30 @@
-;;; ========================================================
-;;; blender-python-mode.el       Dietrich Bollmann, Tokyo 2007/01/09
-;;; 
-;;; Emacs mode for Blender Python development.
-;;; --------------------------------------------------------
-;;; 
-;;; Copyright (C) Dietrich Bollmann
-;;; 
-;;; Author:         Dietrich Bollmann
-;;; Maintainer:     Dietrich Bollmann
-;;; Keywords:       emacs, python, blender.
-;;; Contributor(s): Marc Weber
-;;; 
-;;; This file is part of the Blender Python Mode for emacs.
-;;; 
-;;; The Blender Python Mode is free software; you can redistribute it
-;;; and/or modify it under the terms of the GNU General Public License
-;;; as published by the Free Software Foundation; either version 3, or
-;;; (at your option) any later version.
-;;; 
-;;; The Blender Python Mode is distributed in the hope that it will be
-;;; useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-;;; of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;; 
-;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;;; Boston, MA 02110-1301, USA.
-;;; 
-;;; --------------------------------------------------------
+;;; blender-python-mode.el --- Emacs mode for Blender Python development
+
+;; Copyright (C) 2007  Dietrich Bollmann
+
+;; Author:         Dietrich Bollmann
+;; Maintainer:     Dietrich Bollmann
+;; Keywords:       emacs, python, blender.
+;; Contributor(s): Marc Weber
+
+;; This file is part of the Blender Python Mode for emacs.
+
+;; The Blender Python Mode is free software; you can redistribute it
+;; and/or modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; The Blender Python Mode is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+;; of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
 
 (require 'cl)
 (require 'python)
